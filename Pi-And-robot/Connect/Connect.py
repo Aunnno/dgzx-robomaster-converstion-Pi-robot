97df337f62abf3073621a94cbb66a22500cdf842
Aunnno <2307738723@qq.com>
2025-04-06 10:19:14 +0000

    树莓派连接代码v1.0.0 完成了基本连接框架

--- Pi-And-robot/Connect/Connect.py
+++ Pi-And-robot/Connect/Connect.py
@@ -14,20 +14,24 @@ ser.timeout = 0.2
 
 # 打开串口
 ser.open()
+def conv_init():#握手函数
+    accept=int(ser.readline()) #选择永久堵塞，因为你树莓派在比赛的时候毕竟是一直开着的
+    if accept==1001:
+        print("连接成功")
+        ser.write(1001)
+        return 1
+    
+def heart(num):#心跳函数
+    accept=ser.readline(1.5)#1.5秒超时
+    if len(accept)!=0:
+       ser.write(num)
+    else:
+        print("连接丢失")
 
-while True:
-
-        
-
-        recv = int(ser.readline())#接受初始连接代码
-        if recv==1001:#判断是否连接成功
-                ser.writelines("1")
-                print("Open successfully")
-        else:
-                print("Fail to connect")
-
-
-        
+def connect(num):
+    conv_init()
+    while heart(num):#这个num主要是给heart函数传数据，主程序定义
+        continue
 
 # 关闭串口
 ser.close()
