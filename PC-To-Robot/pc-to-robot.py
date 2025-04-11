f6e0c437989be29c20cf73b36fec5e8a1bfa17c1
Aunnno <2307738723@qq.com>
2025-04-11 17:01:43 +0000

    选手端代码v1.1.5 小改，改进了heart()函数调用 计时器让我明天再写我要累死了

--- PC-To-Robot/pc-to-robot.py
+++ PC-To-Robot/pc-to-robot.py
@@ -1,3 +1,4 @@
+import time
 serial_ctrl.serial_config(115200,"cs8","none",1)#设置基本连接参数
 def conv_init():#握手
      serial_ctrl.write_number(1001) #尝试连接，发送测试字符
@@ -9,7 +10,7 @@ def conv_init():#握手
      
 def heart():#心跳
     serial_ctrl.write_number(1002)#发送心跳代码
-    accept=serial_ctrl.read_line(1.5)#接收返回值
+    accept=serial_ctrl.read_line(2)#接收返回值
     if len(accept)==0:
         return False
     else:
@@ -32,22 +33,26 @@ def send(skill_num):#发送技能编号
 def inv(accept):
     if accept!=False:    #为什么要写报错提示呢主要是为了方便调试
         if accept==1001:
-                return 1
+            print("握手成功")
+            return 1
+        elif accept==1002:
+            print("心跳")
+            return 1
         elif accept==1102:
-                print("技能启动失败")
-                return 0
+            print("技能启动失败")
+            return 0
         elif accept==1103:
-                print("技能执行失败")
-                return 0
+            print("技能执行失败")
+            return 0
         elif accept==1003:
-                print("技能启动成功")
-                return 1
+            print("技能启动成功")
+            return 1
         elif accept==1004:
-                print("技能关闭")
-                return 0
+            print("技能关闭")
+            return 0
         elif accept==1005:
-                print("通信结束")
-                return 0
+            print("通信结束")
+            return 0
     else:
         print("连接丢失")
         return 0
@@ -61,6 +66,7 @@ def main():
        return 0
    accept=heart()
    while inv(accept):
+        sleep(2.0)
         accept=heart()
         continue
       
